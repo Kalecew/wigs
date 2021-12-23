7a0e6e25c231babf5bb847aaee45250a4fff4f0c
Kalecew <adamlovelacee@gmail.com>
2021-12-23 19:35:10 +0300

    backup

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ $(function(){
 	// nav-toggle active
 	$("#nav-toggle").on("click", function(){
 		$(this).toggleClass("nav-toggle--active");
+		$('#header-nav').toggleClass("header-nav--active");
+	});
+
+	// sidebar-toggle active
+	$("#sidebar-toggle").on("click", function(e){
+		e.stopPropagation();
+		$('#nav-toggle').removeClass("nav-toggle--active");
+		$('#sidebar-wrap').toggleClass("sidebar-wrap--active");
+	});
+	$("body").on("click", function(){
+		$('#sidebar-wrap').removeClass("sidebar-wrap--active");
 	});
 
 }); 
